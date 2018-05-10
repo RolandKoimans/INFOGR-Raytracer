53360d01070c2b12b22b026cae10732f599a9d99
Roland Koimans <roland.koimans@gmail.com>
2018-05-10 12:47:25 +0200

    Added basic functionality to Camera.cs, as showed in slides

--- Camera.cs
+++ Camera.cs
@@ -11,6 +11,22 @@ namespace Template
 {
     class Camera
     {
+        Vector3 cameraPos = new Vector3(0, 0, 0);
+        Vector3 viewDirection = new Vector3(0, 0, 1);
+
+        float FOV = 1;
+        Vector3 p1, p2, p3;
+        Vector3 screenCenter = new Vector3(0, 0, 0);
+        
+
+        public Camera()
+        {
+            screenCenter = cameraPos + FOV * viewDirection;
+            p1 = screenCenter + new Vector3(-1, -1, 0);
+            p2 = screenCenter + new Vector3(1, -1, 0);
+            p3 = screenCenter + new Vector3(-1, 1, 0);
+ 
+        }
 
     }
 }
