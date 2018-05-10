df0a96052c8cd435f1280367434c99f12cd4fe60
Thalvarian <a.vanterwisga@students.uu.nl>
2018-05-10 18:07:36 +0200

    basis planes
    plane bottom wordt de bodem en dus het dambord
    distance is afhankelijk van de origin (die in het midden gezet moet worden eerst) dus is nog niet bepaald.

--- Plane.cs
+++ Plane.cs
@@ -11,6 +11,27 @@ namespace Template
 {
     class Plane : Primitive
     {
+        public Vector3 normal;
+        float distance; // or vector3?
+    
+        public Plane()
+        {            
+            //5 planes needed
 
+            Plane left = new Plane();
+            left.normal = new Vector3(1, 0, 0);
+
+            Plane top = new Plane();
+            top.normal = new Vector3(0, 1, 0);
+
+            Plane back = new Plane();
+            back.normal = new Vector3(0, 0, -1);
+
+            Plane right = new Plane();
+            right.normal = new Vector3(-1, 0, 0);
+
+            Plane bottom = new Plane();
+            bottom.normal = new Vector3(0, -1, 0); // should become the checkered board. 
+        }
     }
 }
