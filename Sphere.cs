8c0951a03f89727db159c72a7989567eb139e943
Thalvarian <a.vanterwisga@students.uu.nl>
2018-05-10 15:20:08 +0200

    bolletje

--- Sphere.cs
+++ Sphere.cs
@@ -13,10 +13,25 @@ namespace Template
     {
         Vector3 spherePos;
         int radius;
+        double x, y, z;
+        int theta, phi;
 
         public Sphere()
         {
-            
+            theta = 0;
+            phi = 0;
+            spherePos = new Vector3(12, 1, 4); // temporary values
+        }
+
+        public void DrawSphere()
+        {
+            while (theta < 2*Math.PI && phi < 2*Math.PI)               
+            {
+                theta++; phi++;
+                x = spherePos.X + radius * (Math.Sin(theta) * Math.Cos(phi));
+                y = spherePos.Y + radius * (Math.Sin(theta) * Math.Sin(phi));
+                z = spherePos.Z + radius * (Math.Cos(theta));
+            }
         }
     }
 }
