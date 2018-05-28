71ec19f0ce090217239f4dbf973e433be6906c63
Roland Koimans <roland.koimans@gmail.com>
2018-05-28 21:54:23 +0200

    Start on FOV

--- Camera.cs
+++ Camera.cs
@@ -14,14 +14,17 @@ namespace Template
         public Vector3 cameraPos = new Vector3(0, 0, 0);
         public Vector3 viewDirection = new Vector3(0, 0, 1);
 
-        float FOV = 1;
+        double FOV;
+        double fovAngle = 20;
         public Vector3 p1, p2, p3;
         public Vector3 screenCenter = new Vector3(0, 0, 0);
 
 
         public Camera()
         {
-            screenCenter = cameraPos + FOV * viewDirection;
+            FOV = 0.5 / Math.Tan(fovAngle);
+            Console.WriteLine(FOV);
+            screenCenter = cameraPos + (float)FOV * viewDirection;
             p1 = screenCenter + new Vector3(-1, -1, 0); // bottom left
             p2 = screenCenter + new Vector3(1, -1, 0); // bottom right
             p3 = screenCenter + new Vector3(-1, 1, 0); // top left
